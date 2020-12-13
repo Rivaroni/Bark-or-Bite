acdca3bc1f452892c116c58f46fd19cde74d0de2
Michael Rivera <michaelrivera9192000@gmail.com>
2020-12-13 08:19:56 -0800

    Initial Commit

---
+++ Bark or Bite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bark or Bite
+//
+//  Created by Michael Rivera on 12/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
