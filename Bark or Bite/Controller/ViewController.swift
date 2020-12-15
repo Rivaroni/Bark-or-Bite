b8fcf4ad47d0a4b81a48db50b46cb53b962c27d6
Michael Rivera <michaelrivera9192000@gmail.com>
2020-12-15 10:56:53 -0800

    Added Pan Gesture functionality to be able to swipe the card view, added a back button as well to see previous images

--- Bark or Bite/Controller/ViewController.swift
+++ Bark or Bite/Controller/ViewController.swift
@@ -9,18 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dogTextLabel: UILabel!
+    @IBOutlet weak var chatButton: UIButton!
+    @IBOutlet weak var thumbView: UIImageView!
     
     var imageManager = ImageManager()
+    
+    var previousImages = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Set up delegate to allow view controller to edit
         imageManager.delegate = self
+        
+        cardView.layer.cornerRadius = 10
+        imageView.layer.cornerRadius = 10
     }
-
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
-        imageManager.fetchImage()
+        
+        if sender.currentTitle == "Back"{
+            fetchPrevious()
+            
+        } else {
+            imageManager.fetchImage()
+        }
+        
+    }
+    
+    @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+        
+        let card = sender.view!
+        // point tracks how far u dragged and what direction
+        let point = sender.translation(in: view)
+        let xFromCenter = card.center.x - view.center.x
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+        
+        if xFromCenter > 0 {
+            thumbView.image = UIImage(named: "thumbup")
+            thumbView.tintColor = UIColor.green
+        } else {
+            thumbView.image = UIImage(named: "thumbdown")
+            thumbView.tintColor = UIColor.red
+        }
+        
+        thumbView.alpha = abs(xFromCenter / view.center.x)
+        
+        if sender.state == UIGestureRecognizer.State.ended {
+            UIView.animate(withDuration: 0.5) {
+                card.center = self.view.center
+                self.thumbView.alpha = 0
+            }
+        }
     }
     
 }
@@ -33,7 +75,7 @@ extension ViewController: ImageManagerDelegate{
         DispatchQueue.main.async {
             
             self.imageView.image = self.fetchUIImage(imageString: image.dogImage)
-        
+            
         }
     }
     
@@ -54,11 +96,20 @@ extension ViewController{
         let imageData = try! Data(contentsOf: imageURL!)
         
         if let image = UIImage(data: imageData){
-        
+            
+            previousImages.append(image)
+            
             return image
         }
         
         return nil
+    }
+    
+    func fetchPrevious() {
+        if previousImages.count > 1{
+            previousImages.removeLast()
+            imageView.image = previousImages.last
+        }
     }
 }
 
