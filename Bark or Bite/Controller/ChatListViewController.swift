a5a24cf4ed357d3e92d26d815fac5d42db761265
Michael Rivera <michaelrivera9192000@gmail.com>
2020-12-18 09:13:20 -0800

    Linked up new table view controller as well as renamed and refactor some code

---
+++ Bark or Bite/Controller/ChatListViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ChatViewController.swift
+//  Bark or Bite
+//
+//  Created by Michael Rivera on 12/16/20.
+//
+
+import UIKit
+import Firebase
+import FirebaseFirestore
+
+class ChatListViewController: UIViewController{
+    
+    @IBOutlet weak var chatTableView: UITableView!
+
+    let dataBase = Firestore.firestore()
+    var chatList = [ChatList]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        chatTableView.dataSource = self
+        
+        chatTableView.rowHeight = 125
+  
+        loadList()
+       
+    }
+    
+}
+
+//MARK: - TableView loading and functions
+
+extension ChatListViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "List", for: indexPath)
+        
+        cell.imageView?.image = chatList[indexPath.row].image
+        cell.textLabel?.text = chatList[indexPath.row].dogName
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toChat", sender: self)
+    }
+    
+    func loadList(){
+        
+        dataBase.collection("chatList")
+            .order(by: "mostRecent", descending: true)
+            .addSnapshotListener { (querySnapshot, error) in
+                
+                self.chatList = []
+                
+                if let e = error {
+                    print("Issue retrieving data from Firestore \(e)")
+                } else {
+                    if let snapshotDocuments = querySnapshot?.documents{
+                        for doc in snapshotDocuments {
+                            let data = doc.data()
+                            if let dogName = data["dogName"] as? String, let dogImage = data["imageURL"] as? String{
+                                let imageURL = URL(string: dogImage)
+                                let imageData = try! Data(contentsOf: imageURL!)
+                                if let image = UIImage(data: imageData){
+                                    let newListEntry = ChatList(dogName: dogName, image: image)
+                                    self.chatList.append(newListEntry)
+                                }
+                                
+                                DispatchQueue.main.async {
+                                    self.chatTableView.reloadData()
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+    }
+    
+}
--- Bark or Bite/Controller/ChatViewController.swift
+++ Bark or Bite/Controller/ChatViewController.swift
@@ -2,90 +2,22 @@
 //  ChatViewController.swift
 //  Bark or Bite
 //
-//  Created by Michael Rivera on 12/16/20.
+//  Created by Michael Rivera on 12/18/20.
 //
 
 import UIKit
-import Firebase
-import FirebaseFirestore
 
-class ChatViewController: UIViewController{
-    
-    @IBOutlet weak var chatTableView: UITableView!
-    @IBOutlet weak var chatTextField: UITextField!
-    
-    let dataBase = Firestore.firestore()
-    var chatList = [ChatList]()
-    
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        chatTableView.dataSource = self
-        
-        chatTableView.rowHeight = 125
-  
-        loadList()
-       
-    }
-    
-    @IBAction func sendButtonPressed(_ sender: UIButton) {
-    }
-    
-}
 
-//MARK: - TableView loading and functions
-
-extension ChatViewController: UITableViewDataSource, UITableViewDelegate{
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return chatList.count
+        // Do any additional setup after loading the view.
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "List", for: indexPath)
-        
-        cell.imageView?.image = chatList[indexPath.row].image
-        cell.textLabel?.text = chatList[indexPath.row].dogName
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "toChat", sender: self)
-    }
-    
-    func loadList(){
-        
-        dataBase.collection("chatList")
-            .order(by: "mostRecent", descending: true)
-            .addSnapshotListener { (querySnapshot, error) in
-                
-                self.chatList = []
-                
-                if let e = error {
-                    print("Issue retrieving data from Firestore \(e)")
-                } else {
-                    if let snapshotDocuments = querySnapshot?.documents{
-                        for doc in snapshotDocuments {
-                            let data = doc.data()
-                            if let dogName = data["dogName"] as? String, let dogImage = data["imageURL"] as? String{
-                                let imageURL = URL(string: dogImage)
-                                let imageData = try! Data(contentsOf: imageURL!)
-                                if let image = UIImage(data: imageData){
-                                    let newListEntry = ChatList(dogName: dogName, image: image)
-                                    self.chatList.append(newListEntry)
-                                }
-                                
-                                DispatchQueue.main.async {
-                                    self.chatTableView.reloadData()
-                                }
-                                
-                            }
-                        }
-                    }
-                }
-            }
+    @IBAction func sendButtonTapped(_ sender: UIButton) {
     }
     
 }
